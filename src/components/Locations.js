a5286a7cccaf6591cbde50fd6421776dbf5dcec7
Brubaker Wills <brubakerwills@gmail.com>
2017-01-09 15:15:44 -0500

    spell checked

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -7,6 +7,7 @@ class Locations extends Component {
   render () {
     return <div>
       <h2>Our Locations</h2>
+      <p className='info'>(click a location for map and hours)</p>
       {data.locations.map((location, i) => {
         return <section key={i}>
           <h3 className='shops'><Link to={`/locations/${location.slug}`}>{location.name}</Link></h3>
